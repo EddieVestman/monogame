fd842119b20cfb2baefa934a4852f0e134c73284
Eddie Vestman <edve02001@utb.vaxjo.se>
2019-12-18 11:20:15 +0100

    changed color to blue

--- Template/Template/Game1.cs
+++ Template/Template/Game1.cs
@@ -89,7 +89,7 @@ namespace Template
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Pink);
+            GraphicsDevice.Clear(Color.Blue);
             spriteBatch.Begin();
 	        spriteBatch.Draw(bulldog, bulldogPos, Color.White);
 	        spriteBatch.End();
